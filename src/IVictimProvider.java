73d9fdfc5a3e922d758f81d4d2e005285fa4f5ab
TahirAtakanCan <atkan4202@gmail.com>
2023-10-10 23:03:17 +0300

    first commit

---
+++ src/IVictimProvider.java
@@ -0,0 +1,2 @@
+public class IVictimProvider {
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Village simulator");
+        System.out.println("(Where there's nothing to eat and your neighbors are monsters.)\n");
+
+        
+
+
+    }
+}
